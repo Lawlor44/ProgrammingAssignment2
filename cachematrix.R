29975559df275ad29e2155aecb0ae079ce9d3245
Lawlor44 <lawlor@sbcglobal.net>
2014-06-21 13:53:56 -0700

    Code Skeleton

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,29 @@
 ##makeCacheMatrix crates and set a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+}
 
+cacheSolve <- function(x, ...) {
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
 }
 
 
